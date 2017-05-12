842b0cb4342824c66f4deff2d5045563e7329348
fzed51 <fzed51+git@gmail.com>
2017-05-12 11:14:42 +0200

    script de publication

---
+++ Publish-It.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+    Publie la brève en cours et prépare la brève suivante
+.EXAMPLE
+    Publish-It
+#>
+
+[CmdletBinding()]
+[OutputType([Void])]
+Param (
+    [switch] $NoNewVersion
+)
+
+begin {
+    [string]$FileInfo = "./info.json"
+    [psobject]$info = get-content $FileInfo | ConvertFrom-Json
+    $git = Get-Command "git"
+} 
+
+process {
+    [string]$commit_msg = "dernier commit de n" + $info.version.courante
+    [string]$tag_name = "publish_n" + $info.version.courante
+    &$git add --all
+    &$git commit -m $commit_msg
+    &$git tag $tag_name
+    &$git checkout master
+
+    [int]$derniereVersion = $info.version.courante
+    [int]$CouranteVersion = $info.version.courante + 1
+    $info.publication = $(Get-Date -Format s)
+    $info.version.derniere = $LastVersion
+    $info.version.courante = $CouranteVersion
+    ConvertTo-Json $info | Set-Content $FileInfo
+    $commit_msg = "Creation de n" + $info.version.courante
+    &$git add --all
+    &$git commit -m $commit_msg
+
+    [string]$NewBranch = "n" + $info.version.courante
+    &$git checkout -b $NewBranch
+
+}
+
+end {
+}
